1f43fd98206eee66670c88319cbf7c4260775be4
Young Hahn <young@mapbox.com>
2015-09-03 03:59:15 -0400

    More fixes.

--- test/ec2ppuku.test.sh
+++ test/ec2ppuku.test.sh
@@ -9,6 +9,7 @@ if [ -z "$out" ]; then
     echo "ok - default limit: 90%, noop"
 else
     echo "not ok - default limit: 90%, noop"
+    CODE=1
 fi
 
 out=$(Ec2ppukuLimit=100 ec2ppuku)
@@ -16,6 +17,7 @@ if [ -z "$out" ]; then
     echo "ok - limit: 100%, noop"
 else
     echo "not ok - limit: 100%, noop"
+    CODE=1
 fi
 
 out=$(Ec2ppukuLimit=0 ec2ppuku)
@@ -23,16 +25,19 @@ if echo $out | grep -q "Shutting down..."; then
     echo "ok - limit: 0%, halt"
 else
     echo "not ok - limit: 0%, halt"
+    CODE=1
 fi
 if echo $out | grep -qE "/dev/[0-z]+ use [0-9]+% is above ec2ppuku limit 0%"; then
     echo "ok - limit: 0%, logs troublesome device"
 else
     echo "not ok - limit: 0%, logs troublesome device"
+    CODE=1
 fi
 if echo $out | grep -q "mocked halt"; then
     echo "ok - limit: 0%, calls mocked halt"
 else
     echo "not ok - limit: 0%, calls mocked halt"
+    CODE=1
 fi
 
 exit $CODE
